1db96e29764948cd4b67c0d9b6acedb77d63cf5e
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-01-28 02:19:22 +0100

    Installing phantomjs and casperjs to automate wordpress installation.

--- manifests/default.pp
+++ manifests/default.pp
@@ -62,3 +62,41 @@ exec { "site wordpress":
   ],
   notify => Service [ "apache2" ]
 }
+
+file { "/opt/phantomjs/":
+  ensure => directory
+}
+
+exec { "download phantomjs":
+  command => "/usr/bin/wget -O /opt/phantomjs/phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.6-linux-i686.tar.bz2",
+  user => "root",
+  require => File [ "/opt/phantomjs/" ]
+}
+
+exec { "install phantomjs":
+  command => "/bin/tar -jxvf /opt/phantomjs/phantomjs.tar.bz2 -C /opt/phantomjs/ --strip-components 1",
+  user => "root",
+  require => Exec [ "download phantomjs" ]
+}
+
+exec { "add phantomjs binary":
+  command => "/bin/ln -s /opt/phantomjs/bin/phantomjs /usr/bin/phantomjs",
+  user => "root",
+  require => Exec [ "install phantomjs" ]
+}
+
+file { "/opt/casperjs/":
+  ensure => directory
+}
+
+exec { "download casperjs":
+  command => "/usr/bin/wget -O /opt/casperjs/casperjs.tar.gz https://github.com/n1k0/casperjs/tarball/1.1-beta3",
+  user => "root",
+  require => File [ "/opt/casperjs/" ]
+}
+
+exec { "install casperjs":
+  command => "/bin/tar -zxvf /opt/casperjs/casperjs.tar.gz -C /opt/casperjs/ --strip-components 1",
+  user => "root",
+  require => Exec [ "add phantomjs binary", "download casperjs" ]
+}
